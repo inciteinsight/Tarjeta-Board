4d6033fc230bd1ccd9c5dac7140ce88e5f287bc9
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-03-01 00:22:05 -0500

    Preparing for PostGres

--- server/db/index.js
+++ server/db/index.js
@@ -3,4 +3,27 @@ const db = require('./db')
 // register models
 require('./models')
 
+// To Do List:
+
+// WorshipService
+// - Year
+// - WeekNumber
+// - ServiceType
+// - Local - varchar(50)
+// - Extension - varchar(50)
+
+// Member
+// - ID
+// - Local - varchar(50)
+// - Extension - varchar(50)
+// - AreaGroup - varchar(7)
+// - LastName - varchar (40)
+// - FirstName - varchar (50)
+// - CFO - varchar(10)
+// - Officer - varchar(20)
+// - Gender - Enum (M, F)
+
+// Local
+// - District?
+
 module.exports = db
