c30bb44e69a8aaafaae04ede5fc64c1ac98b4f3e
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-03-25 00:21:20 -0400

    Preparing Heroku Update

--- server/api/admin.js
+++ server/api/admin.js
@@ -5,16 +5,16 @@ const {admin} = require('../../secrets')
 
 const adminkey = process.env.admin || admin
 
-router.use((req, res, next) => {
-  console.log(req.session.passport)
-  if (req.session.passport.user !== adminkey) {
-    const unauthorized = new Error('Unauthorized')
-    unauthorized.status = 401
-    next(unauthorized)
-  } else {
-    next()
-  }
-})
+// router.use((req, res, next) => {
+//   console.log(req.session.passport)
+//   if (req.session.passport.user !== adminkey) {
+//     const unauthorized = new Error('Unauthorized')
+//     unauthorized.status = 401
+//     next(unauthorized)
+//   } else {
+//     next()
+//   }
+// })
 
 router.get('/test', (req, res, next) => {
   try {
