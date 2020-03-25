1dcd4fcaf842477c20ca900889f19f230543e321
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-03-25 00:27:56 -0400

    Final push

--- server/api/admin.js
+++ server/api/admin.js
@@ -1,20 +1,19 @@
 const router = require('express').Router()
-const {Attendance} = require('../db/models')
 const {commandAlpha, commandBeta} = require('../../script/initLoad')
 const {admin} = require('../../secrets')
 
 const adminkey = process.env.admin || admin
 
-// router.use((req, res, next) => {
-//   console.log(req.session.passport)
-//   if (req.session.passport.user !== adminkey) {
-//     const unauthorized = new Error('Unauthorized')
-//     unauthorized.status = 401
-//     next(unauthorized)
-//   } else {
-//     next()
-//   }
-// })
+router.use((req, res, next) => {
+  console.log(req.session.passport)
+  if (req.session.passport.user !== adminkey) {
+    const unauthorized = new Error('Unauthorized')
+    unauthorized.status = 401
+    next(unauthorized)
+  } else {
+    next()
+  }
+})
 
 router.get('/test', (req, res, next) => {
   try {
